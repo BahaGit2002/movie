3df51b7029710924dc06e9b3b1d1f63a75d6af42
BaktiyarJakshylykov <baktiarzaksylykov@gmail.com>
2022-05-10 15:18:22 +0600

    proverka

--- main/admin.py
+++ main/admin.py
@@ -8,10 +8,28 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display_links = ('id', 'name')
 
 
+class ReviewInline(admin.TabularInline):
+    model = Reviews
+    extra = 1
+    readonly_fields = ('name', 'email')
+
+
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
     list_display = ('title', 'Category', 'url', 'draft')
     list_filter = ('Category', 'year')
+    search_fields = ('title', 'Category__name',)
+    inlines = [ReviewInline]
+    save_on_top = True
+    save_as = True
+    list_editable = ('draft', )
+    fields = (("actors", "directors", "genres"), )
+
+
+@admin.register(Reviews)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'parent', 'movie', 'id')
+    readonly_fields = ('name', 'email')
 
 
 admin.site.register(Actor)
@@ -19,7 +37,8 @@ admin.site.register(Genre)
 admin.site.register(MovieShots)
 admin.site.register(Rating)
 admin.site.register(RatingStar)
-admin.site.register(Reviews)
+
+
 
 
 
