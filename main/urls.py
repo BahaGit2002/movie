f32a495869e0f6ed2d3761387492062435ce0dd0
BaktiyarJakshylykov <baktiarzaksylykov@gmail.com>
2022-05-21 13:49:31 +0600

    Register and add a movie

--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.Moviesview.as_view()),
+    path('addmovie/', views.addmovie, name='addmovie'),
     path('<slug:slug>/', views.MovieDetailView.as_view(), name='movie_detail'),
     path("review/<int:pk>", views.AddReview.as_view(), name='add_review'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from django.views.generic.base import View
 from django.views.generic import ListView, DetailView
-from .models import Movie
+from .models import Movie, Actor, Genre, Category
 from .forms import ReviewForm
+from django.contrib import messages
 
 
 # def moviesview(request):
@@ -17,6 +18,12 @@ class Moviesview(ListView):
     template_name = 'pages/movie_list.html'
 
 
+# class Addmovie(ListView):
+#     '''Список фильмов'''
+#     model = Movie
+#     queryset = Movie.objects.filter(draft=False)
+#     template_name = 'pages/addmovie.html'
+
 class MovieDetailView(DetailView):
     model = Movie
     slug_field = 'url'
@@ -29,7 +36,6 @@ class MovieDetailView(DetailView):
 #         return render(request, 'pages/movie_detail.html', context={
 #             'movie_detail': movie
 #         })
-
 
 
 class AddReview(View):
@@ -46,3 +52,38 @@ class AddReview(View):
             form.save()
         return redirect(movie.get_absolute_url())
 
+
+def addmovie(request):
+    if request.method == 'GET':
+        category = Category.objects.all()
+        actors = Actor.objects.all()
+        genre = Genre.objects.all()
+        context = {
+            'category': category,
+            'actors': actors,
+            'genre': genre
+        }
+        return render(request, 'pages/addmovie.html', context)
+    if request.method == 'POST':
+        title = request.POST['title']
+        tagline = request.POST['tagline']
+        discriptions = request.POST['discriptions']
+        year = request.POST['year']
+        world_premiere = request.POST['world_premiere']
+        country = request.POST['country']
+        actors_id = request.POST['actors']
+        directors_id = request.POST['directors']
+        genre_id = request.POST['genre']
+        budget = request.POST['budget']
+        category_id = request.POST['category']
+        fees_in_world = request.POST['fees_in_world']
+        fees_in_usa = request.POST['fees_in_usa']
+        url = request.POST['url']
+        poster = request.POST['poster']
+        new_movie = Movie(title=title, tagline=tagline, discriptions=discriptions, year=year,
+                          world_premiere=world_premiere, country=country, actors_id=actors_id,
+                          directors_id=directors_id, genre_id=genre_id, budget=budget, category_id=category_id,
+                          fees_in_world=fees_in_world, fees_in_usa=fees_in_usa, url=url, poster=poster)
+        messages.success(request, 'Фильм добавлена')
+        new_movie.save()
+        return redirect('addmovie')
